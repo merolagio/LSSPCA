261f92a48f046631d2db571938f9dd8f160be0eb
merolagio <merol@Gio_Spectre>
2021-02-23 11:45:26 +0800

    first commit

---
+++ R/data.R
@@ -0,0 +1,112 @@
+#' Baseball hitters career and 1986 season total statistics
+#'
+#' 16 statistics of 263 major league hitters some of the
+#' overall career and others relative to the 1986 season. Available at StatLib.
+#' The matrix has a block structure, defined by season offensive play, career
+#' offensive play and season defensive play.
+#'
+#'
+#' @name hitters
+#' @docType data
+#' @format A \emph{263} by \emph{16} matrix. The variables are centered
+#' and standardized to unit norm.
+#' \describe{
+#' \item{TAB_86}{times at bat in 1986}
+#' \item{HIT_86}{hits in 1986}
+#' \item{HR_86}{home runs in 1986}
+#' \item{RUN_86}{runs in 1986}
+#' \item{RB_86}{runs batted-in in 1986}
+#' \item{WAL_86}{walks in 1986}
+#' \item{WAL}{walks during his career}
+#' \item{PO_86}{put outs in 1986}
+#' \item{ASS_86}{assists in 1986}
+#' \item{ERR_86}{errors in 1986}
+#' \item{YC}{years in the major leagues}
+#' \item{TAB}{times at bat during his career}
+#' \item{HIT}{hits during his career}
+#' \item{HR}{home runs during his career}
+#' \item{RUN}{runs during his career}
+#' \item{RUNB}{runs batted-in during his career}
+#' }
+#' @source \url{http://lib.stat.cmu.edu/datasets/baseball.data}
+#' @keywords datasets
+NULL
+
+
+#' Baseball hitters statistics labels reference table
+#'
+#' This data frame provides descriptive labels for the variables in the
+#' hitters datasets matching the short labels.
+#'
+#' @name hitters_labels
+#' @docType data
+#' @format A dataframe with columns
+#' \describe{
+#' \item{short.namethe}{labels ised in plots and tables}
+#' \item{label}{the explanatory name}
+#' \item{type}{whether offensive year 1986, defensive year 86 or offensive over the whole career}
+#' }
+#' @source \url{http://lib.stat.cmu.edu/datasets/baseball.data}
+#' @keywords datasets
+NULL
+
+#' Holzinger student's ability data
+#'
+#'Holzinger and Swineford (1939) data is widely cited. This dataset
+#' includes only the 12 tests on the Grant-White School students used in Ferrara et al .
+
+#' @name holzinger
+#' @docType data
+#' @format A \emph{263} by \emph{17} matrix. The first 16 variables are centered
+#' and standardized to unit norm.
+#'
+#' \strong{spatial} (SPL)
+#' \describe{
+#' \item{visual}{Visual perception test, a nonlanguage multiple-choice test of spatial relations }
+#' \item{cubes}{Cubes test, spatial relations}
+#' \item{flags}{Lozenges test, a visual imagery test in two or three dimensions}
+#' }
+#' \strong{verbal} (VBL)
+#' \describe{
+#' \item{paragraph}{Paragraph comprehension test, comprehension as measured by completion and multiple-choice questions}
+#' \item{sentence}{Sentence completion test, a multiple-choice test in which “correct” answers reflect good judgment on the part of the subject}
+#' \item{wordm}{Word meaning test, a multiple-choice vocabulary test}
+#' }
+#' \strong{speed} (SPD)
+#' \describe{
+#' \item{addition}{Addition test, speed of adding pairs of one-digit numbers}
+#' \item{counting}{Counting groups of dots test, 4–7 dots, arranged in random patterns to be counted by subject}
+#' \item{straight}{Straight and curved capitals test, a series of capital letters to be distinguished between those composed of straight lines only and those containing curved lines}
+#' }
+#' \strong{mathematical} (MTH)
+#' \describe{
+#' \item{deduct}{Deduction test, logical deduction test using the symbols and the letters}
+#' \item{numeric}{Numerical puzzles test, a numerical deduction test, the object being to supply four numbers which will produce four given answers employing the operations of addition, multiplication, or division}
+#' \item{series}{Series completion test, from a series of five numbers, the subject is supposed to deduce the rule of procedure from one number to the next and thus supply the sixth number in the series}
+#' }
+#' @source The complete set of data can be found, for example in the R package MBESS.
+
+#' @references
+#' Holzinger, K. J. and Swineford, F. A. (1939). A study in factor analysis: The stability of a bi-factor solution. Supplementary Education Monographs, 48. University of Chicago.
+#'
+#' Ferrara, C., Martella, F., and Vichi, M. (2019). Probabilistic disjoint principal component analysis. Multivariate Behavioral Research, 54(1):47–61.
+#' @keywords datasets
+NULL
+
+
+#' Holzinger student's ability data labels reference table
+#'
+#' This data frame provides descriptive labels for the variables in the
+#' Holzinger student's ability data matching the short labels used.
+#'
+#' @name holzinger_labels
+#' @docType data
+#' @format A dataframe with columns
+#' \describe{
+#' \item{Test}{Name of each test}
+#' \item{ Ability}{Short label for the abilities}
+#' \item{Long_Ability}{Long name of th abilities}
+#' }
+#' @keywords datasets
+NULL
+
---
+++ R/lsspca.R
@@ -0,0 +1,386 @@
+
+##------------------------------------------------------------##
+## lsspca FUNCTION FOR TUTORIAL PAPER
+## Author Giovanni Merola please acknowledge my work
+## email giovanni.merola@xjtlu.edu.cn
+##------------------------------------------------------------##
+
+
+
+#' @title Computes LS SPCA components using different variable selection algorithms
+#'
+#' @description  For each component, the variables are selected so as to explain
+#' a percentage \emph{alpha} of the vexp by the corresponding principal component.
+#' \emph{ind_blocks} is a list containing the indices for each component,
+#'
+#' @param X The data matrix or data.frame.
+#' @param alpha Real in [0,1]. percentage of variance of the PCs explained by the sparse component.
+#' @param maxcard an integer vector or an integer. Missing values filled with last value.
+#' @param ncomps number of components to compute
+#' @param spcaMethod how LS SPCA components are computed:
+#' 'u' for uncorrelated, 'c' for correlated and 'p' for projection
+#' @param subsetSelection how the variables for each component are selected
+#' 'seqrep' stepwise, 'exhaustive' all subsets 'backward', 'forward', 'lasso'
+#' @param force.in NULL or list of indices that must be in component. not for lasso. [NULL]
+#' @param force.out NULL or list of indices cannot be in component. [NULL]
+#' @param selectfromthese NULL or list of indices from which model chosen. [NULL]
+#' @param lsspca_forLasso use lsspca with indices selected with lasso or just the lasso regression
+#' @param lasso_penalty real between 0 and 1, , 0-> ridge regression, 1 -> lasso
+#' @return a list
+#' \describe{
+#' \item{loadings}{Matrix with the loadings scaled to unit \eqn{L_2} norm.}
+#' \item{contributions}{Matrix of loadings scaled to unit \eqn{L_1} norm.}
+#' \item{ncomps}{integer number of components computed.}
+#' \item{cardinality}{Vector with the cardinalities of each loadings.}
+#' \item{ind}{List with the indices of the non-zero loadings for each component.}
+#' \item{loadlist}{A list with only the nonzero ladings for each component.}
+#' \item{vexp}{Vector with the \% variance explained by each component.}
+#' \item{vexpPC}{Vector with the \% variance explained by each principal component.}
+#' \item{cvexp}{Vector with the \% cumulative variance explained by each component.}
+#' \item{rcvexp}{Vector with the \% proportion of cumulative variance explained by each component to that explained by the PCs.}
+#' \item{scores}{the SPCs scores.}
+#' \item{PCloadings}{Matrix with the PCs loadings scaled to unit \eqn{L_2} norm. }
+#' \item{PCs scores}{the PCs scores.}
+#' \item{method}{method used to compute the loadings}
+#' \item{corComp}{Matrix of correlations among the sparse components. Only if ncomps > 1.}
+#' \item{Call}{The called with its arguments.}
+#' }
+
+#' @examples
+#' \dontrun{
+#' library(LSSPCA)
+#' data(hitters_data)
+#'
+#' dim(hitters)
+#' ## USPCA 95
+#' hit_uspca95 = lsspca(X = hitters, alpha = 0.95, ncomps = 4,
+#'                      spcaMethod = "u", subsectSelection = "e")
+#' #> Warning message:
+#' #>  In log(vr) : NaNs produced
+#' ## the warnings come from the variable selection, don't worry
+#'
+#' ## summaries
+#' t(data.frame(card = hit_uspca95$cardinality,
+#'              cvexp = round(hit_uspca95$cvexp, 2),
+#'              rcvexp = round(hit_uspca95$rcvexp, 2)))
+#'
+#' ## print loadings individually
+#' lapply(hit_uspca95$loadlist, function(x) round(x, 2))
+#' ## print contributions individually
+#' lapply(hit_uspca95$loadlist, function(x) round(x/sum(abs(x)), 2))
+#'
+#' ## plot PC and USPC loadings
+#' par(mfrow = c(1, 2))
+#' barplot(-hit_uspca95$PCloadings[, 1], main = "PCA")
+#' barplot(-hit_uspca95$loadings[, 1], main = "USPCA")
+#' par(mfrow = c(1,1))
+#'
+#' ## Holzinger data
+#' data(holzinger_data)
+#' dim(holzinger)
+#'
+#' ## CSPCA
+#' hol_cspca95 = lsspca(X = holzinger, alpha = 0.95, ncomps = 4,
+#'                      spcaMethod = "c", subsectSelection = "e")
+#'
+#' ## summaries
+#' t(data.frame(card = hol_cspca95$cardinality,
+#'              cvexp = round(hol_cspca95$cvexp, 2),
+#'              rcvexp = round(hol_cspca95$rcvexp, 2)))
+#'
+#' ## print loadings
+#' lapply(hol_cspca95$loadlist, function(x) round(x, 2))
+#' ## print contributions
+#' lapply(hol_cspca95$loadlist, function(x) round(x/sum(abs(x)), 2))
+#'
+#' ## correlation between SPCs
+#' round(hol_cspca95$corComp, 2)
+#'
+#' ## plot contributions
+#' barplot(-hol_cspca95$contributions[, 1])
+#'
+#' ## SPCs scores against PC scores
+#' plot(hol_cspca95$scores[, 1], hol_cspca95$PCscores[, 1], pch = 16)
+#' regline = lm(hol_cspca95$PCscores[, 1] ~ hol_cspca95$scores[, 1]- 1)$coef
+#' abline(a = 0, b = regline, col = 2)
+#'
+#'
+#' ## SPCA on each ability separately
+#' h_groups = lapply(seq(1, 10, 3), function(x) x:(x + 2))
+#'
+#' ## projection SPCA
+#' hol_block_spca95 = lsspca(X = holzinger, alpha = 0.95, ncomps = 4,
+#'                      spcaMethod = "p", subsectSelection = "e",
+#'                      selectfromthese = h_groups)
+#'
+#' ## summaries
+#' t(data.frame(card = hol_block_spca95$cardinality,
+#'              cvexp = round(hol_block_spca95$cvexp, 2),
+#'              rcvexp = round(hol_block_spca95$rcvexp, 2)))
+#'
+#' ## print loadings
+#' lapply(hol_block_spca95$loadlist, function(x) round(x, 2))
+#'
+#' ## print contributions
+#' lapply(hol_block_spca95$loadlist, function(x) round(x/sum(abs(x)), 2))
+#'
+#' ## correlation between SPCs
+#' round(hol_block_spca95$corComp, 2)
+#'
+#' ## plot the contributions for each SPC
+#' par(mfrow = c(2, 2))
+#' for(k in 1:4){
+#'   barplot(-hol_block_spca95$contributions[, k])
+#' }
+#' par(mfrow = c(1, 1))
+#' }
+#'
+#' @author Giovanni Merola
+#'
+#' @export
+lsspca <- function(X, alpha = 0.95, maxcard = 0, ncomps = 0, spcaMethod = c("u", "c", "p"),
+                   scalex = FALSE,
+                   subsetSelection = c("exhaustive", "seqrep", "backward", "forward", "lasso"),
+                   force.in = NULL, force.out = NULL, selectfromthese = NULL,
+                   lsspca_forLasso = TRUE, lasso_penalty = 0.5) {
+
+  ##----------------------------------------##
+  ## Creating the environment and error checking
+  ##----------------------------------------##
+
+  p <- ncol(X)
+  n <- nrow(X)
+  if (is.null(colnames(X)))
+    colnames(X) <- paste0("V", 1:p)
+  ## convert selectfromthese to force out
+  if (is.list(selectfromthese)) {
+    fx <- function(x, m) (1:m)[-x]
+    force.out <- lapply(selectfromthese, fx, m = p)
+  }
+  if (is.list(force.in) | is.list(force.out)) {
+    ncomps <- max(length(force.in), length(force.out))
+    if (length(force.in) < ncomps)
+      force.in <- c(force.in, vector("list", ncomps - length(force.in)))
+    if (length(force.out) < ncomps)
+      force.out <- c(force.out, vector("list", ncomps - length(force.out)))
+  }
+  else if (ncomps == 0) {
+    ncomps <- p
+  }
+  if (!any(spcaMethod == c("u", "c", "p")))
+    stop("only one of these options allowed for spcaMethod: 'u', 'c', 'p'")
+  if (length(maxcard) > 1)
+    ncomps <- length(maxcard)
+  if (is.vector(maxcard)) {
+    maxcard[maxcard == 0] <- p
+    if (length(maxcard < ncomps)) {
+      lm <- length(maxcard)
+      maxcard <- c(maxcard, rep(maxcard[lm], ncomps - lm))
+    }
+  } else {
+    stop("must pass a vector or an integer as maxcard")
+  }
+  subsetSelection <- switch(stringr::str_sub(subsetSelection[1], 1, 1), e = "exhaustive", b = "backward", f = "forward",
+                             s = "seqrep", l = "lasso")
+  if (is.null(subsetSelection))
+    stop("please pass a valid variable selection option")
+  spcaMethod <- spcaMethod[1]
+
+  if (!requireNamespace("geigen", quietly = TRUE)) {
+    stop("Package \"geigen\" needed for this function to work. Please install it.",
+         call. = FALSE)
+  }
+  if (!requireNamespace("stringr", quietly = TRUE)) {
+    stop("Package \"stringr\" needed for this function to work. Please install it.",
+         call. = FALSE)
+  }
+
+  if (subsetSelection == "lasso")
+    if (!requireNamespace("elasticnet", quietly = TRUE)) {
+      stop("Package \"elasticnet\" needed for lasso selection to work. Please install it.",
+           call. = FALSE)
+    }
+  else
+    if (!requireNamespace("leaps", quietly = TRUE)) {
+      stop("Package \"leaps\" needed for variable selection to work. Please install it.",
+           call. = FALSE)
+    }
+
+  ##-------------------------------------------------------------------##
+  ## create objects for output
+  ##-------------------------------------------------------------------##
+  card <- rep(0, ncomps)
+  ind <- as.list(card)
+  vexp <- rep(0, ncomps)
+  cvexp <- rep(0, ncomps)
+  A <- matrix(0, p, ifelse(ncomps == 0, p, ncomps))
+  contributions <- A
+  loadlist <- as.list(1:p)
+  scores <- matrix(0, n, ncomps)
+  if (spcaMethod[1] == "u")
+    R <- matrix(0, p, ncomps)
+
+  if (is.null(colnames(X)))
+    namx <- paste("Var", 1:p) else namx <- colnames(X)
+
+  ##-------------------------------------------------------------------##
+  ## center and scale the variables
+  ##-------------------------------------------------------------------##
+  if (scalex == TRUE)
+    X <- scale(X) else if (any(abs(colMeans(X)) > 10^-6)) {
+      message("You need to center the columns of X to zero mean, I'll do it for you")
+      X <- scale(X, scale = FALSE)
+    }
+
+  ##-------------------------------------------------------------------##
+  ## compute of the components
+  ##-------------------------------------------------------------------##
+  K <- X
+  nc <- 0
+  j <- 1
+  stopComp <- FALSE
+  while (stopComp == FALSE) {
+    D <- crossprod(K)  #SPCA3::ataC(K)
+    if (j == 1)
+      Cmat <- D
+    r_ee <- eigen(D)  #SPCA3::EigenC(D)
+    pc <- K %*% r_ee$vec[, 1]
+    lambda <- r_ee$val[1]
+    if (j == 1) {
+      vexpPC <- r_ee$val
+      totvar <- sum(r_ee$val)
+      PCloadings = r_ee$vec[, 1:ncomps]
+      PCscores = X %*% PCloadings
+    }
+
+    ## variable selection ==========================
+    if (stringr::str_sub(subsetSelection[1], 1, 1) == "l") {
+      if ((length(force.out) > 0) | (length(force.in) > 0))
+        stop("lasso not implemented with force.out or force.in")
+      else g_fit <- glmnet::glmnet(X, pc, intercept = FALSE, alpha = lasso_penalty)
+      mod_ind <- ifelse(any(g_fit$dev.ratio > alpha), min(which(g_fit$dev.ratio > alpha)), length(g_fit$dev.ratio))
+      coe <- as.numeric(coef(g_fit, s = g_fit$lambda[mod_ind]))[-1]
+      ind[[j]] <- which(coe != 0)
+      card[j] <- length(ind[[j]])
+      a <- coe[ind[[j]]]
+    }
+    else {
+      ssr <- leaps::regsubsets(x = X, y = pc, spcaMethod = subsetSelection[1], nbest = 1, force.in = force.in[[j]],
+                               force.out = force.out[[j]], nvmax = maxcard[j], intercept = FALSE)
+
+      aa <- leaps:::summary.regsubsets(ssr)
+      aa$which <- aa$which[, order(match(colnames(aa$which), colnames(X)))]
+      mrsq <- any(aa$rsq >= alpha)
+      if (mrsq == TRUE)
+        if (spcaMethod == "u")
+          iord <- which((aa$rsq >= alpha) & ((1:length(aa$rsq)) >= j))  ## rsquared
+      else iord <- which(aa$rsq >= alpha)  ## rsquared
+      else {
+        iord <- length(aa$rsq)
+        if (maxcard[j] > min(iord))
+          message(paste("warning: lsspca component ", j, "could not reach Rsquared", alpha))
+      }
+
+      indall <- 1:p
+      ind[[j]] <- indall[aa$which[iord[1], ]]
+      card[j] <- length(ind[[j]])
+    }
+    ##  compute LSSPCs =====================
+    if ((subsetSelection != "l") | ((subsetSelection == "l") & (lsspca_forLasso == TRUE))) {
+      if (length(ind[[j]]) > 1) {
+        Xd <- X[, ind[[j]]]
+        Sd <- crossprod(Xd)
+        if (spcaMethod == "u")
+        {
+          if (j == 1) {
+            M <- tcrossprod(crossprod(Xd, X))
+            ga <- geigen::geigen(M, Sd, symmetric = TRUE)
+            a <- ga$vec[, card[j]]
+          }
+          else {
+            xxd <- crossprod(X, Xd)
+            H <- crossprod(A[, 1:(j - 1), drop = FALSE], xxd)
+            Sm <- solve(Sd)
+            E <- H %*% Sm
+            G <- diag(card[j]) - crossprod(H, solve(tcrossprod(E, H))) %*% E
+            M <- crossprod(tcrossprod(xxd, G))
+            ga <- geigen::geigen(M, Sd)
+            a <- ga$vec[, card[j]]
+          }
+        }
+        else {
+          if (spcaMethod == "c") {
+            M <- crossprod(crossprod(K, Xd))
+            ga <- geigen::geigen(M, Sd, symmetric = TRUE)  ##SPCA3::GenEigenC(M, Sd)
+            a <- ga$vec[, card[j]]
+          }
+          if (spcaMethod == "p") {
+            alm <- lm(pc ~ Xd - 1)
+            a <- alm$coefficients
+          }
+        }
+        ## save results for j-th SPC ==========================
+
+        a <- a/sqrt(sum(a^2))
+        if (all(a <= 0))
+          a <- -a
+        scores[, j] <- Xd %*% a
+        bb <- cor(pc, scores[, j])
+        if (bb < 0) {
+          a <- -a
+          scores[, j] <- -scores[, j]
+        }
+        names(a) <- namx[ind[[j]]]
+        A[ind[[j]], j] <- a
+        contributions[ind[[j]], j] <- a/sum(abs(a))
+        loadlist[[j]] <- a
+      }
+      else {
+        a <- 1
+        scores[, j] <- X[, ind[[j]]]
+        bb <- cor(pc, scores[, j])
+        if (bb < 0) {
+          a <- -a
+          scores[, j] <- -scores[, j]
+        }
+        names(a) <- namx[ind[[j]]]
+        A[ind[[j]], j] <- a
+        contributions[ind[[j]], j] <- a
+        loadlist[[j]] <- a
+      }
+    }
+    ## deflate K
+    if (j <= ncomps)
+      K <- K - tcrossprod(scores[, j]) %*% K/sum(scores[, j]^2)  #spcaTutoPack:::deflXCforR(c(scores[, j]), K)
+    cvexp[j] <- totvar - sum(K^2)
+    vexp[j] <- ifelse(j > 1, cvexp[j] - cvexp[j - 1], cvexp[j])
+    nc <- nc + 1
+    if (j < ncomps)
+      j <- j + 1 else stopComp <- TRUE
+  }
+
+  ##-------------------------------------------------------------------##
+  ## create output
+  ##-------------------------------------------------------------------##
+  ncomps <- nc
+  rownames(contributions) <- namx
+  rownames(A) <- namx
+  dimnames(PCloadings) = dimnames(A)
+  dimnames(PCscores) = dimnames(scores)
+
+
+  out <- list(loadings = A[, 1:ncomps], contributions = contributions,
+              ncomps = ncomps, cardinality = card[1:ncomps], indices = ind,
+              loadlist = loadlist[1:ncomps],
+              vexp = vexp[1:ncomps]/totvar, vexpPC = vexpPC[1:ncomps]/totvar,
+              cvexp = cvexp[1:ncomps]/totvar, rcvexp = cvexp[1:ncomps]/cumsum(vexpPC[1:ncomps]),
+              scores = scores[, 1:ncomps],  PCloadings = PCloadings,
+              PCscores = PCscores, spcaMethod = spcaMethod)
+  if (ncomps > 1) {
+    if (spcaMethod[1] != "u") {
+      out$corComp <- cor(scores)
+    }
+  }
+  out$Call <- match.call()
+  return(out)
+}
